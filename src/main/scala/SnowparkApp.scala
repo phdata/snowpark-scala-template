652566f6b9e06615d886c4678230a61fda1bfc01
Coop <rcoop@phdata.io>
2021-08-04 09:25:11 -0500

    Added LICENSE, cleaned up pom.xml, reformatted

--- src/main/scala/SnowparkApp.scala
+++ src/main/scala/SnowparkApp.scala
@@ -10,72 +10,71 @@ import scala.collection.JavaConverters._
 object SnowparkApp {
 
 
-    def main(args: Array[String]): Unit = {
-        // FIXME - This could probably be more elegant
-        val stream: InputStream = getClass.getResourceAsStream("snowflake.conf")
-        val properties: Properties = new Properties()
-        properties.load(stream)
+  def main(args: Array[String]): Unit = {
+    val stream: InputStream = getClass.getResourceAsStream("snowflake.conf")
+    val properties: Properties = new Properties()
+    properties.load(stream)
 
-        val session = Session.builder.configs(properties.asScala.asJava).create
-        addDeps(session)
+    val session = Session.builder.configs(properties.asScala.asJava).create
+    addDeps(session)
 
-        // Comment or uncomment below to test out different functions
-         createPermanentUdf(session)
-         createInlinePermanentUdf(session)
-         doInteractiveSession(session)
+    // Comment or uncomment below to test out different functions
+    createPermanentUdf(session)
+    createInlinePermanentUdf(session)
+    doInteractiveSession(session)
+  }
+
+  def doInteractiveSession(session: Session): Unit = {
+    val TABLE_NAME = "TEMP_SNOWPARK.DATA.TITANIC_TABLE"
+
+    val df: DataFrame = session.table(TABLE_NAME)
+
+    df.show(5)
+
+  }
+
+
+  def createPermanentUdf(session: Session): Unit = {
+    session.sql("drop FUNCTION if EXISTS udfPerm(Int)").show()
+
+    session.udf.registerPermanent("udfPerm", permanentUdfHandler _, "STG")
+
+    session.sql("select *, udfPerm(*) from values (10)").show()
+  }
+
+  def permanentUdfHandler(i: Int): Int = {
+    i * i
+  }
+
+  def createInlinePermanentUdf(session: Session): Unit = {
+    session.sql("drop FUNCTION if EXISTS udfInlinePerm(Int)").show()
+
+    session.udf.registerPermanent("udfInlinePerm", (i: Int) => {
+      i * 2
+    }, "STG")
+
+    session.sql("select *, udfInlinePerm(*) from values (10)").show()
+  }
+
+  private def addDeps(session: Session): Unit = {
+    val PATH = Paths.get(".", "target", "dependency").toAbsolutePath.toString
+
+    val lst = getListOfFiles(PATH)
+    val filteredLst = lst.filterNot(_.matches("^.*snowpark(_original)?-[0-9.]+\\.jar$"))
+    for (f <- filteredLst) {
+      System.out.println("Adding dep:" + f)
+      session.addDependency(f)
     }
+  }
 
-    def doInteractiveSession(session: Session): Unit = {
-        val TABLE_NAME = "TEMP_SNOWPARK.DATA.TITANIC_TABLE"
-
-        val df: DataFrame = session.table(TABLE_NAME)
-
-        df.show(5)
-
+  def getListOfFiles(dir: String): List[String] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList.map(_.getPath)
+    } else {
+      List[String]()
     }
-
-
-    def createPermanentUdf(session: Session): Unit = {
-        session.sql("drop FUNCTION if EXISTS udfPerm(Int)").show()
-
-        session.udf.registerPermanent("udfPerm", permanentUdfHandler _,  "STG")
-
-        session.sql("select *, udfPerm(*) from values (10)").show()
-    }
-
-    def permanentUdfHandler(i: Int): Int = {
-        i * i
-    }
-
-    def createInlinePermanentUdf(session: Session): Unit = {
-        session.sql("drop FUNCTION if EXISTS udfInlinePerm(Int)").show()
-
-        session.udf.registerPermanent("udfInlinePerm", (i: Int) => {
-            i * 2
-        },  "STG")
-
-        session.sql("select *, udfInlinePerm(*) from values (10)").show()
-    }
-
-    private def addDeps(session: Session): Unit = {
-        val PATH = Paths.get(".", "target", "dependency").toAbsolutePath.toString
-
-        val lst = getListOfFiles(PATH)
-        val filteredLst = lst.filterNot(_.matches("^.*snowpark(_original)?-[0-9.]+\\.jar$"))
-        for(f <- filteredLst) {
-            System.out.println("Adding dep:" + f)
-            session.addDependency(f)
-        }
-    }
-
-    def getListOfFiles(dir: String): List[String] = {
-        val d = new File(dir)
-        if (d.exists && d.isDirectory) {
-             d.listFiles.filter(_.isFile).toList.map(_.getPath)
-        } else {
-             List[String]()
-        }
-    }
+  }
 
 
 }
